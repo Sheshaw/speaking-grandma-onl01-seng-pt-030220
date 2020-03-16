fc4ee1fda55223d5dbf79be559a5e537d66a5034
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-16 00:22:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,13 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
     
-
-
-  
+  elsif phrase == phrase.downcase
+    "HUH?! SPEAK UP, SONNY!"
+    
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    
+    
+  end
 end
